6dbb841fee2c73399acc853871c7cd01d841f0bf
Yao Qi <yaoqiqq@hotmail.com>
2015-02-26 23:33:49 +0800

    删除无用的
    ／／

--- dz/data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 0666c32df0e6a4bc5113b91ad855cca3
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- dz/data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 765c02c7686fe8b0bf12cea3ff0ebb3c
-
-$pluginsetting = array (
-);
-?>
