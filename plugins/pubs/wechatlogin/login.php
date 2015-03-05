ecce639d1990299c652ca18bec03039b1721d100
Yao Qi <yaoqiqq@hotmail.com>
2015-03-05 13:35:11 +0800

    MAC

--- plugins/pubs/wechatlogin/login.php
+++ plugins/pubs/wechatlogin/login.php
@@ -1,5 +1,7 @@
 <?php 
 defined('IN_TS') or die('Access Denied.');
+include 'config.php';
 
+$urlencode=urlencode($url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$appSecret."&code=".$code."&grant_type=authorization_code";);
 $appid='';
 $appscecret='';
